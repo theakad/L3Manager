547e13a3331a600c6c564a2529dbb5a9430304ba
Rodrigo Bezerra <rodrigo.bezerra@actminds.com>
2018-08-19 16:39:29 -0300

    Add project files.

---
+++ L3Manager.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(L3Manager.Web.Startup))]
+namespace L3Manager.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
